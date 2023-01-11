bae16dcac11795bb6241b01b5ee745bd8904ca41
MarkKuziv <kuziv2004@gmail.com>
2023-01-12 01:38:31 +0200

    Make it better

--- src/main/java/com/big/share/bigshare/models/MailModel.java
+++ src/main/java/com/big/share/bigshare/models/MailModel.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class MailModel {
 
     private String email;
@@ -14,4 +11,52 @@ public class MailModel {
     private String phone;
     private String company;
     private String comment;
+
+    public MailModel(String email, String name, String phone, String company, String comment) {
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+        this.company = company;
+        this.comment = comment;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
